51844a834abc4fdab785cf780e925b8d992f10f8
ads-andredesantana <ads.andredesantana@gmail.com>
2020-03-15 10:59:36 -0400

    Ch7
    Adding an event listener - button

--- Projects/Quiz/main.js
+++ Projects/Quiz/main.js
@@ -1,27 +1,36 @@
-// View Object
-const view = {
-    score: document.querySelector('#score strong'),
-    question: document.getElementById('question'),
-    result: document.getElementById('result'),
-    info: document.getElementById('info'),
-    render(target, content, attributes) {
-        for (const key in attributes) {
-            target.setAttribute(key, attributes[key]);
-        }
-        target.innerHTML = content;
-    }
-};
-
 const quiz = [
     { name: "Superman", realName: "Clark Kent" },
     { name: "Wonder Woman", realName: "Diana Prince" },
     { name: "Batman", realName: "Bruce Wayne" },
 ];
 
+// View Object
+const view = {
+    score: document.querySelector('#score strong'),
+    question: document.getElementById('question'),
+    result: document.getElementById('result'),
+    info: document.getElementById('info'),
+    start: document.getElementById("start"),
+    render(target, content, attributes) {
+        for (const key in attributes) {
+            target.setAttribute(key, attributes[key]);
+        }
+        target.innerHTML = content;
+    },
+    show(element) {
+        element.style.display = "block";
+    },
+    hide(element) {
+        element.style.display = "none";
+    }
+};
+
+// Game Object
 const game = {
     start(quiz) {
-        this.questions = [...quiz];
         this.score = 0;
+        this.questions = [...quiz];
+        view.hide(view.start);
         // main game loop
         for (const question of this.questions) {
             this.question = question;
@@ -50,6 +59,8 @@ const game = {
     },
     gameOver() {
         view.render(view.info, `Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
+        view.show(view.start);
     }
 }
-game.start(quiz);
+
+view.start.addEventListener("click", () => game.start(quiz), false);
